1b40dc04d4ae7cac0506c25d8e02c5f2c1d57148
pengcanjun <1642287765@qq.com>
2025-01-20 11:14:30 +0800

    通过url上传图片

--- shuke-picture-be/src/main/java/com/shuke/shukepicturebe/manager/FileManager.java
+++ shuke-picture-be/src/main/java/com/shuke/shukepicturebe/manager/FileManager.java
@@ -4,6 +4,11 @@ import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpStatus;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.http.Method;
 import com.qcloud.cos.model.PutObjectResult;
 import com.qcloud.cos.model.ciModel.persistence.ImageInfo;
 import com.shuke.shukepicturebe.config.CosClientConfig;
@@ -17,9 +22,12 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * @ClassName: FileManager
@@ -82,6 +90,49 @@ public class FileManager {
         }
     }
 
+    public UploadPictureResult UploadPictureByUrl(String fireUrl , String uploadPathPrefix){
+        // 校验图片 根据url
+        validPicture(fireUrl);
+        // 图片上传地址
+        String uuid = RandomUtil.randomString(16);
+
+        /**
+         *  用HUTool 封装的FileUtil.mainName 方法获取不到文件的后缀  比如.jpg .png
+         *  FileUtil 获取全文件名  FileUtil.getName
+         */
+        String originFilename = FileUtil.mainName(fireUrl);
+        String uploadFilename = String.format("%s_%s.%s", DateUtil.formatDate(new Date()), uuid,
+                FileUtil.getSuffix(originFilename));
+        String uploadPath = String.format("/%s/%s", uploadPathPrefix, uploadFilename);
+        File file = null;
+        try {
+            // 创建临时文件
+            file = File.createTempFile(uploadPath, null);
+            HttpUtil.downloadFile(fireUrl,file);
+            // 上传图片
+            PutObjectResult putObjectResult = cosManager.putPictureObject(uploadPath, file);
+            ImageInfo imageInfo = putObjectResult.getCiUploadResult().getOriginalInfo().getImageInfo();
+            // 封装返回结果
+            UploadPictureResult uploadPictureResult = new UploadPictureResult();
+            int picWidth = imageInfo.getWidth();
+            int picHeight = imageInfo.getHeight();
+            double picScale = NumberUtil.round(picWidth * 1.0 / picHeight, 2).doubleValue();
+            uploadPictureResult.setPicName(FileUtil.mainName(originFilename));
+            uploadPictureResult.setPicWidth(picWidth);
+            uploadPictureResult.setPicHeight(picHeight);
+            uploadPictureResult.setPicScale(picScale);
+            uploadPictureResult.setPicFormat(imageInfo.getFormat());
+            uploadPictureResult.setPicSize(FileUtil.size(file));
+            uploadPictureResult.setUrl(cosClientConfig.getHost() + "/" + uploadPath);
+            return uploadPictureResult;
+        } catch (Exception e) {
+            log.error("图片上传到对象存储失败", e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "上传失败");
+        } finally {
+            this.deleteTempFile(file);
+        }
+    }
+
     /**
      * 校验文件
      *
@@ -98,6 +149,55 @@ public class FileManager {
         // 允许上传的文件后缀
         final List<String> ALLOW_FORMAT_LIST = Arrays.asList("jpeg", "jpg", "png", "webp");
         ThrowUtils.throwIf(!ALLOW_FORMAT_LIST.contains(fileSuffix), ErrorCode.PARAMS_ERROR, "文件类型错误");
+    }
+
+    public void validPicture(String fireUrl){
+        ThrowUtils.throwIf(StrUtil.isBlank(fireUrl),ErrorCode.PARAMS_ERROR,"文件地址不能为空");
+        try {
+            // 1.验证url 地址是否合法 / 验证url格式是否正确
+            new URL(fireUrl);
+        }
+        catch (MalformedURLException e){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "文件地址格式不正确" );
+        }
+
+        // 2. 校验 URL 协议
+        ThrowUtils.throwIf(!(fireUrl.startsWith("http://") || fireUrl.startsWith("https://")),ErrorCode.PARAMS_ERROR,
+                "仅支持HTTP协议和HTTPS协议的文件地址");
+        // 3. 发送HEAD请求以验证文件是否存在
+        HttpResponse response = null;
+        try {
+            // 此处不能用get请求 get请求回获取完整的文件
+            response = HttpUtil.createRequest(Method.HEAD , fireUrl).execute();
+            if(response.getStatus() != HttpStatus.HTTP_OK){
+                return;
+            }
+            // 4. 检验文件类型
+            String contentType = response.header("Content-Type");
+            if(StrUtil.isNotBlank(contentType)){
+                // 允许的图片类型
+                final List<String> ALLOW_CONTENT_TYPES = Arrays.asList("image/jpeg","image/jpg","image/png","image/webp");
+                ThrowUtils.throwIf(!ALLOW_CONTENT_TYPES.contains(contentType.toLowerCase()),ErrorCode.PARAMS_ERROR,
+                        "文件类型错误");
+            }
+            // 5. 文件存在，文件大小校验
+            String contentLengthStr = response.header("Content-Length");
+            if (StrUtil.isNotBlank(contentLengthStr)) {
+                try {
+                    long contentLength = Long.parseLong(contentLengthStr);
+                    // 限制图片大小为1MB
+                    final long ONE_MB = 1024 * 1024;
+                    ThrowUtils.throwIf(contentLength > 2 * ONE_MB, ErrorCode.PARAMS_ERROR, "文件大小不能超过 2MB");
+                } catch (NumberFormatException e) {
+                    throw new BusinessException(ErrorCode.PARAMS_ERROR, "文件大小格式异常");
+                }
+            }
+        }finally {
+            // 记得释放资源
+            if (response != null) {
+                response.close();
+            }
+        }
     }
 
     /**
