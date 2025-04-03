616fb348700a96ed06b1e28f8293410c62734558
pengcanjun <1642287765@qq.com>
2025-04-03 10:01:06 +0800

    请求包装类 和 请求包装过滤器

---
+++ shuke-picture-be/src/main/java/com/shuke/shukepicturebe/config/HttpRequestWrapperFilter.java
@@ -0,0 +1,38 @@
+package com.shuke.shukepicturebe.config;
+
+
+import cn.hutool.http.ContentType;
+import cn.hutool.http.Header;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author 舒克、舒克
+ * @date 2025/4/3 09:44
+ * @description
+ * 请求包装过滤器
+ */
+@Order(1)
+@Component
+public class HttpRequestWrapperFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest servletRequest = (HttpServletRequest) request;
+            String contentType = servletRequest.getHeader(Header.CONTENT_TYPE.getValue());
+            if (ContentType.JSON.getValue().equals(contentType)) {
+                // 可以再细粒度一些，只有需要进行空间权限校验的接口才需要包一层
+                chain.doFilter(new RequestWrapper(servletRequest), response);
+            } else {
+                chain.doFilter(request, response);
+            }
+        }
+    }
+
+}
+
---
+++ shuke-picture-be/src/main/java/com/shuke/shukepicturebe/config/RequestWrapper.java
@@ -0,0 +1,73 @@
+package com.shuke.shukepicturebe.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.*;
+
+/**
+ * @author 舒克、舒克
+ * @date 2025/4/3 09:42
+ * @description
+ * 包装请求，使 InputStream 可以重复读取
+ */
+@Slf4j
+public class RequestWrapper extends HttpServletRequestWrapper {
+
+    private final String body;
+
+    public RequestWrapper(HttpServletRequest request) {
+        super(request);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (InputStream inputStream = request.getInputStream(); BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            char[] charBuffer = new char[128];
+            int bytesRead = -1;
+            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                stringBuilder.append(charBuffer, 0, bytesRead);
+            }
+        } catch (IOException ignored) {
+        }
+        body = stringBuilder.toString();
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body.getBytes());
+        return new ServletInputStream() {
+            @Override
+            public boolean isFinished() {
+                return false;
+            }
+
+            @Override
+            public boolean isReady() {
+                return false;
+            }
+
+            @Override
+            public void setReadListener(ReadListener readListener) {
+            }
+
+            @Override
+            public int read() throws IOException {
+                return byteArrayInputStream.read();
+            }
+        };
+
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(this.getInputStream()));
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+}
+
